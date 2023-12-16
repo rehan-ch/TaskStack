d2f3c7cb61a9e2a756f2f28f27ccfe0f44f617fc
Muhammad Rehan <rehanch642@gmail.com>
2023-12-16 23:56:33 +0500

    tasks component

---
+++ src/components/Tasks/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default const Tasks = ()=>{
+  return (
+    <View>
+        <Text>
+            Tasks
+        </Text>
+    </View>
+  )
+}
